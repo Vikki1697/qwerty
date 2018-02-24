9c3f41caf600cbd16e7b49963e031600a32b8ab4
Vikki1697 <35902485+Vikki1697@users.noreply.github.com>
2018-02-24 11:43:54 +0200

    fix

--- task2/js/action.js
+++ task2/js/action.js
@@ -1,9 +1,34 @@
 //Show or Hide sidebar
 document.getElementById("button").onclick = function() {
     var elem = document.getElementsByClassName('sidebar');
-    var classElem = elem[0].getAttribute('class');
+    setVisibility(elem, 'sidebar item-on btn-side-size');
+    var subMenu = document.getElementsByClassName('sub-menu');
+    if (subMenu[0].hidden){
+    	subMenu[0].hidden = false;
+    } else {
+    	subMenu[0].hidden = true;
+    }
+};
+//Show or Hide font-list
+document.getElementById("font-p").onclick = function() {
+	var elem = document.getElementsByClassName('sub-list');
+	setVisibility(elem, 'sub-list item-on');
+};
+//Show or Hide color-list
+document.getElementById("background-p").onclick = function() {
+	var elem = document.getElementsByClassName('sub-list-c');
+	setVisibility(elem, 'sub-list-c item-on');
+};
+//Show or Hide size-list
+document.getElementById("size-p").onclick = function() {
+	var elem = document.getElementsByClassName('sub-list-s');
+	setVisibility(elem, 'sub-list-s item-on');
+};
+//Show or Hide function
+function setVisibility(elem, options){
+	var classElem = elem[0].getAttribute('class');
 
-    if (classElem == 'sidebar item-on btn-side-size'){
+	if (classElem == options){
     	for (var i = 0; i < elem.length; i++) {
     		elem[i].className = elem[i].className.replace('item-on', 'item-off');
     	}
@@ -12,57 +37,12 @@ document.getElementById("button").onclick = function() {
     		elem[i].className = elem[i].className.replace('item-off', 'item-on');
     	}
     }
-};
+}
 //Delete last <p>
 document.getElementById("delete-p").onclick = function() {
 	var p = document.getElementsByClassName('content-p');
 	var cnt = p.length; 
 	p[cnt-1].remove();
-};
-//Show or Hide font-list
-document.getElementById("font-p").onclick = function() {
-	var elem = document.getElementsByClassName('sub-list');
-	var classElem = elem[0].getAttribute('class');
-
-	if (classElem == 'sub-list item-on'){
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-on', 'item-off');
-    	}
-    }else{
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-off', 'item-on');
-    	}
-    }
-};
-//Show or Hide color-list
-document.getElementById("background-p").onclick = function() {
-	var elem = document.getElementsByClassName('sub-list-c');
-	var classElem = elem[0].getAttribute('class');
-
-	if (classElem == 'sub-list-c item-on'){
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-on', 'item-off');
-    	}
-    }else{
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-off', 'item-on');
-    	}
-    }
-};
-//Show or Hide size-list
-document.getElementById("size-p").onclick = function() {
-	var elem = document.getElementsByClassName('sub-list-s');
-	var classElem = elem[0].getAttribute('class');
-
-	if (classElem == 'sub-list-s item-on'){
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-on', 'item-off');
-    	}
-    }else{
-    	for (var i = 0; i < elem.length; i++) {
-    		elem[i].className = elem[i].className.replace('item-off', 'item-on');
-    	}
-    }
 };
 //Change font-size
 document.getElementById("btn-size").onclick = function() {
@@ -75,8 +55,7 @@ document.getElementById("btn-size").onclick = function() {
 		}
 	}else{
 		alert('Ошибка!');
-	}
-	
+	}	
 };
 
 function setFont(){
@@ -119,6 +98,8 @@ function changeBackground(color) {
 	}
 }
 
-function changeSize(){
-
+function isCorrect(inpt){
+	var val = inpt.value;
+	val = val.replace(/[^\d]/g, '');
+	document.getElementById('inpt-size').value = val;
 }
